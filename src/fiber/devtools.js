b43aaca90cca6c6181e876e4792c83af84822eb8
Dustan Kasten <dustan.kasten@gmail.com>
2017-12-04 09:20:59 -0500

    Move devtools setup to try/catch with a warning

--- src/fiber/devtools.js
+++ src/fiber/devtools.js
@@ -1,20 +1,26 @@
 /* global __REACT_DEVTOOLS_GLOBAL_HOOK__ */
 
-const defineProperty = Object.defineProperty;
-defineProperty(global, 'WebSocket', {
-  value: require('ws')
-});
-defineProperty(global, 'window', {
-  value: global
-});
-const {connectToDevTools} = require('react-devtools-core');
+try {
+  const defineProperty = Object.defineProperty;
+  defineProperty(global, 'WebSocket', {
+    value: require('ws')
+  });
+  defineProperty(global, 'window', {
+    value: global
+  });
+  const {connectToDevTools} = require('react-devtools-core');
 
-connectToDevTools({
-  isAppActive() {
-    // Don't steal the DevTools from currently active app.
-    return true;
-  },
-  host: 'localhost',
-  port: 8097,
-  resolveRNStyle: null, // TODO maybe: require('flattenStyle')
-});
+  connectToDevTools({
+    isAppActive() {
+      // Don't steal the DevTools from currently active app.
+      return true;
+    },
+    host: 'localhost',
+    port: 8097,
+    resolveRNStyle: null, // TODO maybe: require('flattenStyle')
+  });
+} catch (err) {
+  if (process.env.NODE_ENV !== 'production') {
+    console.warn('WARNING: the `ws` package must be installed to use `react-devtools`.');
+  }
+}
