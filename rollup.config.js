7b6e611557807456f2035f1998002e13a686e862
Yomguithereal <guillaumeplique@gmail.com>
2021-02-06 16:10:29 +0100

    Refreshing prettier

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,5 @@
 import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
-import external from 'rollup-plugin-peer-deps-external';
 import resolve from 'rollup-plugin-node-resolve';
 
 import pkg from './package.json';
